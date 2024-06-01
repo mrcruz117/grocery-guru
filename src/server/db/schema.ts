9bcab3d2dc943012cec3e65f9890f499d8286b64
mrcruz117 <mrcruz1989@gmail.com>
2024-06-01 16:52:14 -0400

    changes to db schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -3,14 +3,16 @@
 
 import { sql } from "drizzle-orm";
 import {
+  boolean,
   index,
   integer,
   pgTableCreator,
   serial,
   timestamp,
+  unique,
   varchar,
 } from "drizzle-orm/pg-core";
-import { number } from "zod";
+import { number, string } from "zod";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -20,41 +22,44 @@ import { number } from "zod";
  */
 export const createTable = pgTableCreator((name) => `g-g_${name}`);
 
+export const recipes = createTable(
+  "recipes",
+  {
+    id: serial("id").primaryKey(),
+    name: varchar("name", { length: 256 }),
+    description: varchar("description", { length: 1024 }),
+
+    // userId from Clerk
+    // user_x7x7x7x7x7x7x7x7x7x7x7x7x7x
+    created_by: varchar("created_by", { length: 64 }),
+    private: boolean("private"),
+  },
+  // (example) => ({
+  //   nameUnique: unique("name_unique").on(example.name),
+  // }),
+);
+
 export const ingredients = createTable(
   "ingredients",
   {
     id: serial("id").primaryKey(),
     name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updatedAt", { withTimezone: true }),
   },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  }),
+
+  // how to add an index
+  // (example) => ({
+  //   nameIndex: index("name_idx").on(example.name),
+  // }),
 );
 
-export const recipes = createTable("recipes", {
+export const ingredientMeasureUnits = createTable("ingredient_measure_units", {
   id: serial("id").primaryKey(),
   name: varchar("name", { length: 256 }),
-  createdAt: timestamp("created_at", { withTimezone: true })
-    .default(sql`CURRENT_TIMESTAMP`)
-    .notNull(),
-  updatedAt: timestamp("updatedAt", { withTimezone: true }),
 });
 
-export const recipeIngredients = createTable(
-  "recipe_ingredients",
-  {
-    recipeId: integer("recipe_id").references(() => recipes.id),
-    ingredientId: integer("ingredient_id").references(() => ingredients.id),
-    amount: integer("amount"),
-  },
-  (example) => ({
-    recipeIngredientIndex: index("recipe_ingredient_idx").on(
-      example.recipeId,
-      example.ingredientId,
-    ),
-  }),
-);
+export const recipeIngredients = createTable("recipe_items", {
+  recipeId: integer("recipe_id").references(() => recipes.id),
+  ingredientId: integer("ingredient_id").references(() => ingredients.id),
+  amount: integer("amount"),
+  unitType: integer("unit_type").references(() => ingredientMeasureUnits.id),
+});
